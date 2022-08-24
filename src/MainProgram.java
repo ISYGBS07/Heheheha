43a0b92be785087326129319315d4c67391ef242
jianalex <alex20jiang07@gmail.com>
2022-08-24 15:04:33 +0200

    Alex ist schwul haha

---
+++ src/MainProgram.java
@@ -0,0 +1,16 @@
+public class MainProgram {
+    public static void main(String[] args){
+        //System.out.print("Hi Isy wie gehts dir so?");
+        boolean a = true;
+        int i = 0;
+        while(a){
+            System.out.println("Hi");
+            i = i++;
+        }
+        if (i == 1);{
+            a = false;
+            System.out.println("Alex ist schwul");
+        }
+        //System.out.print("Hi Alex wie gehts dir so?");
+    }
+}
